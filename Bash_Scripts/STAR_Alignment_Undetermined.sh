d334093af8f8e214082ad71cea4de5a461876f88
Ben Carr <bcarr@login2.talapas.uoregon.edu>
2024-10-03 11:25:05 -0700

    File organization

--- Bash_Scripts/STAR_Alignment_Undetermined.sh
+++ Bash_Scripts/STAR_Alignment_Undetermined.sh
@@ -15,7 +15,7 @@ conda list -n QAA
 --outSAMunmapped Within KeepPairs \
 --alignIntronMax 1000000 --alignMatesGapMax 1000000 \
 --readFilesCommand zcat \
---readFilesIn /home/bcarr/bgmp/bioinfo/Bi623/QAA/Undetermined_trimmed_1U.fastq.gz /home/bcarr/bgmp/bioinfo/Bi623/QAA/Undetermined_trimmed_2P.fastq.gz \
+--readFilesIn /home/bcarr/bgmp/bioinfo/Bi623/QAA/Undetermined_trimmed_1U.fastq.gz /home/bcarr/bgmp/bioinfo/Bi623/QAA/Undetermined_trimmed_2P.fastq.gz \  #NOTE: Should have used 1P, not 1U!
 --genomeDir /home/bcarr/bgmp/bioinfo/Bi623/QAA/STAR_Database \
 --outFileNamePrefix Undetermined_S0_L008
 
